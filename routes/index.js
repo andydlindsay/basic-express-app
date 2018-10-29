20a135a27228132a5f7c7cb439fde5c12571e119
Andy Lindsay <andydlindsay@gmail.com>
2018-10-29 00:16:39 +0000

    added basic routing in routes folder

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = (app) => {
+  app.get('/', (req, res) => {
+    res.render('pages/index');
+  });
+
+  app.get('/about', (req, res) => {
+    res.render('pages/about');
+  });
+};
